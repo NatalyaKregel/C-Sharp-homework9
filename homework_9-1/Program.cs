3fafc114a9af4ea418e3fa231b958800e14f083f
Nata Kregel <natik.kregel@gmai.com>
2022-09-17 00:45:00 +0300

    Добавили дз №9

---
+++ homework_9-1/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Задача 64: Задайте значения M и N. 
+Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+M = 4; N = 8. -> ""4, 6, 7, 8"" */
+
+int Count(int min, int max)
+{   
+    if( min < max ) 
+    {   
+        Console.Write($"{min},");
+        return min + Count(++min, max);
+    }
+        if( min > max ) 
+    {   
+        Console.Write($"{min},");
+        return min + Count(--min, max);
+    }
+    else 
+    {   
+        Console.Write($"{min}");
+        return min;
+    }
+}
+
+Console.WriteLine("Введите первое число:");
+int min = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите последнее число:");
+int max = int.Parse(Console.ReadLine());
+
+int countNumbers = Count(min, max);
---
+++ homework_9-2/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Задача 66: Задайте значения M и N. 
+Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+int Sum(int min, int max)
+{   
+    if( min < max ) 
+    {   
+        Console.Write($"{min},");
+        return min + Sum(++min, max);
+    }
+        if( min > max ) 
+    {   
+        Console.Write($"{min},");
+        return min + Sum(--min, max);
+    }
+    else 
+    {   
+        Console.Write($"{min}");
+        return min;
+    }
+}
+
+Console.WriteLine("Введите первое число:");
+int min = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите последнее число:");
+int max = int.Parse(Console.ReadLine());
+
+int sumNumbers = Sum(min, max);
+Console.WriteLine($" - Сумма представленных чисел равна {sumNumbers}");
